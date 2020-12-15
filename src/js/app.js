a02e120366a467ba5c2271fd76c910d4f07ae34b
dogger-work <dogger.work@gmail.com>
2020-12-16 00:37:01 +0200

    0.0.1 - Added header and started footer components. Fixed grid

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,34 @@
 import '../sass/app.sass'
 
+
+//  Header
+
+const $header         = $('header')
+const $headerMenuBtn  = $('#header-menu-btn')
+const $menu           = $('.menu')
+
+
+function scrollPage() {
+    if (window.pageYOffset > 0) {
+        $header.addClass('is-scroll')
+    } else {
+        $header.removeClass('is-scroll')
+    }
+
+    /*
+    setTimeout(function () {
+        if (window.innerWidth < 992) $menu.css('padding-top', $header.outerHeight(true))
+    }, 300)
+    */
+}
+
+
+function toggleHeaderMenu() {
+    $headerMenuBtn.toggleClass('is-active')
+    $header.toggleClass('is-menu-open')
+    $menu.toggleClass('is-open')
+    document.body.classList.toggle('menu-opened')
+}
+
+$($headerMenuBtn).on('click', toggleHeaderMenu)
+$(window).on('resize scroll load', scrollPage)
